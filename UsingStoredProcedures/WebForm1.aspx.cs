df21c73e1d0fb73b25e7ac98d08326b52fa864a5
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-31 12:19:43 +0530

    Using Stored Procedures with LINQ

---
+++ UsingStoredProcedures/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UsingStoredProcedures
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetData()
+        {
+            SampleDataContext dbContext = new SampleDataContext();
+            GridView1.DataSource = dbContext.GetEmployees();
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+    }
+}
