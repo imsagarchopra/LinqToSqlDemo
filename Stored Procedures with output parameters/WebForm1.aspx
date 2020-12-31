63159bddd14a2df0172924f349ba2883a6040ca8
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-31 12:49:02 +0530

    Using stored procedures with output parameters

---
+++ Stored Procedures with output parameters/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Stored_Procedures_with_output_parameters.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <br />
+            <asp:Label ID="lblDept" runat="server"></asp:Label>
+            <br />
+            <asp:Button ID="btnGetEmployeesByDepartment" runat="server" OnClick="btnGetEmployeesByDepartment_Click" Text="Get Employees By Department" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Stored Procedures with output parameters/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Stored_Procedures_with_output_parameters
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetEmployeesByDepartment_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                string deptName = string.Empty;
+                GridView1.DataSource = dbContext.GetEmployeesByDepartment(1, ref deptName);
+                GridView1.DataBind();
+
+                lblDept.Text = "Department Name = " + deptName;
+            }
+        }
+    }
+}
