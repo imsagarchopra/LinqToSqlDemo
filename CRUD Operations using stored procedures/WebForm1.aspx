15ce55e84d3974b5bd066201f1aeea3eed6d4c5c
Sagar Chopra <Chopra.sagar89@outlook.com>
2020-12-31 12:34:48 +0530

    CRUD Operations using stored procedures

---
+++ CRUD Operations using stored procedures/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CRUD_Operations_using_stored_procedures.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+        <asp:Button ID="btnGetData" runat="server" OnClick="btnGetData_Click" Text="Get Data" />
+&nbsp;<asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert" />
+&nbsp;<asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+&nbsp;<asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+    </form>
+</body>
+</html>
---
+++ CRUD Operations using stored procedures/WebForm1.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_Operations_using_stored_procedures
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetData()
+        {
+            SampleDataContext dbContext = new SampleDataContext();
+            GridView1.DataSource = dbContext.Employees;
+            GridView1.DataBind();
+        }
+
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee newEmployee = new Employee
+                {
+                    FirstName = "Tim",
+                    LastName = "T",
+                    Gender = "Male",
+                    Salary = 55000,
+                    DepartmentId = 1
+                };
+
+                dbContext.Employees.InsertOnSubmit(newEmployee);
+                dbContext.SubmitChanges();
+            }
+
+            GetData();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee employee = dbContext.Employees.SingleOrDefault(emp => emp.ID == 8);
+                employee.Salary = 65000;
+                dbContext.SubmitChanges();
+            }
+            GetData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                Employee employee = dbContext.Employees.SingleOrDefault(emp => emp.ID == 8);
+                dbContext.Employees.DeleteOnSubmit(employee);
+                dbContext.SubmitChanges();
+            }
+            GetData();
+        }
+    }
+}
